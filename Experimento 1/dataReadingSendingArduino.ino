ba42d20aabb3dcf658b5b53a08a5f037adc0f455
jssosa10 <js.sosa10@uniandes.edu.co>
2017-08-30 18:57:59 -0500

    ino completo funcionando

---
+++ Experimento 1/dataReadingSendingArduino.ino
@@ -0,0 +1,280 @@
+#include <string.h>
+#include <DHT11.h>
+#include <SparkFunTSL2561.h>
+#include <Wire.h>
+/********************************* MQ-7 Gas Sensor *********************************/
+
+/************************Hardware Related Macros************************************/
+#define         MQ_PIN                       (0)     //define which analog input channel you are going to use
+#define         RL_VALUE                     (10)    //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR          (9.83)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                                     //which is derived from the chart in datasheet
+
+/***********************Software Related Macros************************************/
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+                                                     //cablibration phase
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
+                                                     //normal operation
+
+/**********************Application Related Macros**********************************/
+#define         GAS_LPG                      (0)
+#define         GAS_CO                       (1)
+#define         GAS_H2                       (2)
+#define         GAS_CH4                      (3)
+#define         GAS_Al                       (4)
+
+/*****************************Globals***********************************************/
+float           LPGCurve[3]  =  {1.7,0.95,-0.13};   //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve. 
+                                                    //data format:{ x, y, slope}; point1: (lg50, 0.95), point2: (lg400, 0.70) 
+float           COCurve[3]  =  {1.7,0.20,-0.66};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg50, 0.20), point2: (lg400,  -1.05) 
+float           H2Curve[3] =  {1.7,0.11,-0.73};     //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg50, 0.11), point2: (lg400,  -1.28)
+float           CH4Curve[3]  =  {1.7,1.15,-0.10};   //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve. 
+                                                    //data format:{ x, y, slope}; point1: (lg50, 1.15), point2: (lg400, 0.95) 
+float           AlCurve[3]  =  {1.7,1.23,-0.08};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve. 
+                                                    //data format:{ x, y, slope}; point1: (lg50, 1.23), point2: (lg400, 1.08) 
+float           Ro           =  10;                 //Ro is initialized to 10 kilo ohms
+DHT11 dht11(2);
+SFE_TSL2561 light;
+
+// Varibles golbales:
+
+boolean gain;     // Gain setting, 0 = X1, 1 = X16;
+unsigned int ms;
+
+ String tempArray[2] = {"", ""};
+ // variable temporal de conteo
+ int i = 0;
+ // preparacion del proceso
+ void setup() { 
+   // Abre puerto serial y lo configura a 9600 bps
+     Serial.begin(9600);
+      light.begin();
+
+  // Get factory ID from sensor:
+  // (Just for fun, you don't need to do this to operate the sensor)
+
+  unsigned char ID;
+  
+  if (light.getID(ID))
+  {
+   // Serial.print("Got factory ID: 0X");
+    //Serial.print(ID,HEX);
+    //Serial.println(", should be 0X5X");
+  }
+  // Most library commands will return true if communications was successful,
+  // and false if there was a problem. You can ignore this returned value,
+  // or check whether a command worked correctly and retrieve an error code:
+  else
+  {
+    //byte error = light.getError();
+    //printError(error);
+  }
+
+  // The light sensor has a default integration time of 402ms,
+  // and a default gain of low (1X).
+  
+  // If you would like to change either of these, you can
+  // do so using the setTiming() command.
+  
+  // If gain = false (0), device is set to low gain (1X)
+  // If gain = high (1), device is set to high gain (16X)
+
+  gain = 0;
+
+  // If time = 0, integration will be 13.7ms
+  // If time = 1, integration will be 101ms
+  // If time = 2, integration will be 402ms
+  // If time = 3, use manual start / stop to perform your own integration
+
+  unsigned char time = 2;
+
+  // setTiming() will set the third parameter (ms) to the
+  // requested integration time in ms (this will be useful later):
+  
+ // Serial.println("Set timing...");
+  light.setTiming(gain,time,ms);
+
+  // To start taking measurements, power up the sensor:
+  
+  //Serial.println("Powerup...");
+  light.setPowerUp();
+   //Serial.print("Calibrating...\n");                
+  Ro = MQCalibration(MQ_PIN);                       //Calibrating the sensor. Please make sure the sensor is in clean air 
+                                                    //when you perform the calibration                    
+ // Serial.print("Calibration is done...\n"); 
+ //Serial.print("Ro=");
+ // Serial.print(Ro);
+ // Serial.print("kohm");
+ // Serial.print("\n");
+ }
+ // ejecuta el procesamiento del sensor
+ void loop() {
+   // lee el valor del sensor de temperatura en Volts
+   int err;
+   float temp, hum;
+   err = dht11.read(hum, temp);
+   float g = MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_CO);
+   int sound;
+   sound = analogRead(3); 
+   delay(ms);
+   // Retrieve the data from the device:
+
+  unsigned int data0, data1;
+  
+  light.getData(data0,data1);
+    // getData() returned true, communication was successful
+    
+   // Serial.print("data0: ");
+    //Serial.print(data0);
+   // Serial.print(" data1: ");
+    //Serial.print(data1);
+  
+    // To calculate lux, pass all your settings and readings
+    // to the getLux() function.
+    
+    // The getLux() function will return 1 if the calculation
+    // was successful, or 0 if one or both of the sensors was
+    // saturated (too much light). If this happens, you can
+    // reduce the integration time and/or gain.
+    // For more information see the hookup guide at: https://learn.sparkfun.com/tutorials/getting-started-with-the-tsl2561-luminosity-sensor
+  
+    double lux;    // Resulting lux value
+    boolean good;  // True if neither sensor is saturated
+    
+    // Perform lux calculation:
+
+    good = light.getLux(gain,ms,data0,data1,lux);
+   // se transforma el dato int de temperatura a un char
+  // tempArray[0] = String(tempC);
+   // Envia los datos uno por uno del arreglo del sensor por puerto serial
+   //for (i=0; i<2; i++){
+     // imprime el elemento del arreglo por el puerto serial
+     //Serial.print(tempArray[i]);
+     // espacio para diferenciar elementos en el arreglo
+    // if (i < 1){
+       //Serial.print("\t");      
+     //}
+   //}
+   // final de la trama de datos
+   Serial.print(temp);
+   Serial.print("-");
+   Serial.print(sound/1024.0);
+   Serial.print("-");
+   Serial.print(g);
+   Serial.print("-");
+   Serial.print(lux);
+   Serial.println("");
+
+   delay(1000);
+
+ }
+ /****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/ 
+float MQResistanceCalculation(int raw_adc)
+{
+  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+
+/***************************** MQCalibration ****************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use  
+         MQResistanceCalculation to calculates the sensor resistance in clean air 
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about 
+         10, which differs slightly between different sensors.
+************************************************************************************/ 
+float MQCalibration(int mq_pin)
+{
+  int i;
+  float val=0;
+
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQResistanceCalculation(analogRead(mq_pin));
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+
+  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+                                                        //according to the chart in the datasheet 
+
+  return val; 
+}
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/ 
+float MQRead(int mq_pin)
+{
+  int i;
+  float rs=0;
+
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(analogRead(mq_pin));
+    delay(READ_SAMPLE_INTERVAL);
+  }
+
+  rs = rs/READ_SAMPLE_TIMES;
+
+  return rs;  
+}
+
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which 
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/ 
+int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+  if ( gas_id == GAS_LPG ) {
+     return MQGetPercentage(rs_ro_ratio,LPGCurve);
+  } else if ( gas_id == GAS_CO ) {
+     return MQGetPercentage(rs_ro_ratio,COCurve);
+  } else if ( gas_id == GAS_H2 ) {
+     return MQGetPercentage(rs_ro_ratio,H2Curve);
+  }  else if ( gas_id == GAS_CH4 ) {
+     return MQGetPercentage(rs_ro_ratio,CH4Curve);
+  }  else if ( gas_id == GAS_Al ) {
+     return MQGetPercentage(rs_ro_ratio,AlCurve);
+  }    
+
+  return 0;
+}
+
+/*****************************  MQGetPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm) 
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a 
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic 
+         value.
+************************************************************************************/ 
+int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+{
+  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
