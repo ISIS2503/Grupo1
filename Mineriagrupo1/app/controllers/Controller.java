4d016e127233e42f721dc0a72b1ad525854de8a9
Diego Castro Losada <df.castro12@uniandes.edu.co>
2017-09-24 17:01:03 -0500

    clases

---
+++ Mineriagrupo1/app/controllers/Controller.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by df.castro12 on 24/09/2017.
+ */
+public class Controller {
+}
---
+++ Mineriagrupo1/app/entities/Entity.java
@@ -0,0 +1,7 @@
+package entities;
+
+/**
+ * Created by df.castro12 on 24/09/2017.
+ */
+public class Entity {
+}
