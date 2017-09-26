55a83dc63ed0b50dd153106bda56f45752ec5c75
jssosa10 <js.sosa10@uniandes.edu.co>
2017-09-26 10:50:17 -0500

    ejemplo uso request body

--- LosMineralesPlay/app/controllers/UserController.scala
+++ LosMineralesPlay/app/controllers/UserController.scala
@@ -21,7 +21,14 @@ class UserController @Inject()(cc: ControllerComponents) extends AbstractControl
     em.close()
     Ok("user test01 record persisted for persistence unit cassandra_pu")
   }
-
+  //Ejemplo uso body
+  def sayHello = Action(parse.json) { request =>
+    (request.body \ "name").asOpt[String].map { name =>
+      Ok("Hello " + name)
+    }.getOrElse {
+      BadRequest("Missing parameter [name]")
+    }
+  }
   def persist(user: User) = Action {
     val em: EntityManager = emf.createEntityManager()
     em.persist(user)
