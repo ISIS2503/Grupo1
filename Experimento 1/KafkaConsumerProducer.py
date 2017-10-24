410c8a7d5659146b942232b6da6d1fbc01516bf7
jssosa10 <js.sosa10@uniandes.edu.co>
2017-10-24 12:14:46 -0500

    vcambios

--- Experimento 1/KafkaConsumerProducer.py
+++ Experimento 1/KafkaConsumerProducer.py
@@ -1,20 +1,25 @@
-from kafka import KafkaProducer
-from kafka import KafkaProducer
-from kafka.errors import KafkaError
-from datetime import datetime
+from kafka import KafkaConsumer
+import datetime
+import requests
 
-consumer = KafkaConsumer(‘alta.piso1.area1’,
+consumer = KafkaConsumer('alta.piso1.area1',
                          group_id='my-group',
-                         bootstrap_servers=['localhost:8090’])
-
+                         bootstrap_servers=['172.24.42.20:8090'])
 location = 1001001
 
 for message in consumer:
-	msg = message.split(“,”) 
-	now = msg[0].split(“:”)[1].replace(“\””,””)
-	temp = msg[1].split(“:”)[2]
-	noise = msg[2].split(“:”)[1]
-	gas = msg[3].split(“:”)[1]
-	ilum = msg[4].split(“:”)[1].split(“}”)[0]
-	sendit = “{’location’:’” + location + “‘, ’temperature’:’” + temp + “‘, ‘gas’:’” + gas + “‘, ’light’:’” + ilum + “‘, ‘sound’:’” + noice + “‘, ‘timestamp:’”+ now+”’}”
-	r = requests.post(’localhost:9000/measurement’, sendit)
+	msg = message.split(',') 
+	payload = {}
+	now = msg[0].split(':')[1]
+	temp = float(msg[1].split(':')[1])
+	noise = float(msg[2].split(':')[1])
+	gas = float(msg[3].split(':')[1])
+	ilum = float(msg[4].split(':')[1].split('}')[0])
+	payload["location"]=location
+	payload["temperature"]=temp
+	payload["gas"]=gas
+	payload["light"]=ilum
+	payload["sound"]=noise
+	payload["timestamp"]=now 
+	r = requests.post("http://172.24.42.34:9000/measurement",json = payload)
+	print(r)
--- envios.py
+++ envios.py
@@ -1,5 +1,16 @@
 import requests
-payload = {"location" : 213423,"temperature": 323.32,"gas" : 32.32,"light" : 323.32,"sound" : 323.32,"timestamp" : "23/12/2017"}
+import json
+d  = 10
+payload = {"location" : d,"temperature": 323.32,"gas" : 32.32,"light" : 323.32,"sound" : 323.32,"timestamp" : "23/12/2017"}
 for i in range(1,100):
 	r = requests.post("http://172.24.42.34:9000/measurement",json = payload)
 	print(r)
+	data = {}
+	data["location"]= i
+	data["temperature"] = 323.32
+	data["gas"]=32.32
+	data["light"]=323.33
+	data["sound"]=323.3
+	data["timestamp"]="23/12/2017"
+	r = requests.post("http://172.24.42.34:9000/measurement",json = data)
+	print(r)s
